9f34a2c79e4317269f66367df8f87a01b5356419
Jan Zwiener <jan@zwiener.org>
2024-09-21 22:01:12 +0200

    WIP: First instruction

--- include/byteboy.h
+++ include/byteboy.h
@@ -21,12 +21,36 @@
  * DEFINES
  ******************************************************************************/
 
-#define BB_MAX_CODE_SIZE       0xFFFF /**< max. number of bytes for ASM
+#define BB_MAX_ROM_SIZE       0xFFFF /**< max. number of bytes for ASM
                                            instructions */
+
+#define BB_MAX_RAM_SIZE        0xFFFF
+
+#define BB_MEM_MASK            0xFFFF
+
 
 /******************************************************************************
  * TYPEDEFS
  ******************************************************************************/
+
+typedef enum {
+    BB_OP_ADD_A_A   = 0x87, // 135
+    BB_OP_ADD_A_B   = 0x80, // 128
+	BB_OP_ADD_A_C   = 0x81, // 129
+	BB_OP_ADD_A_D   = 0x82, // 130
+	BB_OP_ADD_A_E   = 0x83, // 131
+	BB_OP_ADD_A_H   = 0x84, // 132
+	BB_OP_ADD_A_L   = 0x85, // 133
+	BB_OP_ADD_A_HL  = 0x86, // 134
+	BB_OP_ADD_A_N   = 0xC6, // 198
+} bb_op_code_t;
+
+typedef enum {
+    BB_CARRY_FLAG      = 0x10,
+    BB_HALF_CARRY_FLAG = 0x20,
+    BB_SUBTRACT_FLAG   = 0x40,
+    BB_ZERO_FLAG       = 0x80,
+} bb_flags_t;
 
 typedef struct bb_reg_s
 {
@@ -51,9 +75,9 @@ typedef struct bb_reg_s
 typedef struct bb_state_s
 {
     bb_reg_t reg;
-    uint8_t code[BB_MAX_CODE_SIZE];
-
-
+    uint8_t rom[BB_MAX_ROM_SIZE];
+    uint8_t ram[BB_MAX_RAM_SIZE];
+    int pc; /**< program counter */
 } bb_state_t;
 
 /******************************************************************************
