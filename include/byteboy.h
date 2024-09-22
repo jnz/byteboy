5afdab0ec38c3fb476ef6cd38a9e196540998dda
Jan Zwiener <jan@zwiener.org>
2024-09-22 11:18:11 +0200

    ADC added

--- include/byteboy.h
+++ include/byteboy.h
@@ -21,20 +21,15 @@
  * DEFINES
  ******************************************************************************/
 
-#define BB_MAX_ROM_SIZE       0xFFFF /**< max. number of bytes for ASM
-                                           instructions */
-
-#define BB_MAX_RAM_SIZE        0xFFFF
-
 #define BB_MEM_MASK            0xFFFF
-
 
 /******************************************************************************
  * TYPEDEFS
  ******************************************************************************/
 
+/* Source: https://rgbds.gbdev.io/docs/v0.8.0/gbz80.7 */
+
 typedef enum {
-    BB_OP_ADD_A_A   = 0x87, // 135
     BB_OP_ADD_A_B   = 0x80, // 128
 	BB_OP_ADD_A_C   = 0x81, // 129
 	BB_OP_ADD_A_D   = 0x82, // 130
@@ -42,7 +37,21 @@ typedef enum {
 	BB_OP_ADD_A_H   = 0x84, // 132
 	BB_OP_ADD_A_L   = 0x85, // 133
 	BB_OP_ADD_A_HL  = 0x86, // 134
+    BB_OP_ADD_A_A   = 0x87, // 135
 	BB_OP_ADD_A_N   = 0xC6, // 198
+
+    BB_OP_ADC_A_B   = 0x88,
+    BB_OP_ADC_A_C   = 0x89,
+    BB_OP_ADC_A_D   = 0x8A,
+    BB_OP_ADC_A_E   = 0x8B,
+    BB_OP_ADC_A_H   = 0x8C,
+    BB_OP_ADC_A_L   = 0x8D,
+    BB_OP_ADC_A_HL  = 0x8E,
+    BB_OP_ADC_A_A   = 0x8F,
+    BB_OP_ADC_A_N   = 0xCE,
+
+    BB_OP_HALT      = 0x76,
+
 } bb_op_code_t;
 
 typedef enum {
@@ -64,10 +73,6 @@ typedef struct bb_reg_s
              *    bit 4: carry */
             uint8_t a, f, b, c, d, e, h, l;
         };
-        struct
-        {
-            uint16_t af, bc, de, hl;
-        };
         uint8_t arr[8];
     };
 } bb_reg_t;
@@ -75,9 +80,10 @@ typedef struct bb_reg_s
 typedef struct bb_state_s
 {
     bb_reg_t reg;
-    uint8_t rom[BB_MAX_ROM_SIZE];
-    uint8_t ram[BB_MAX_RAM_SIZE];
+    uint8_t* rom;
+    uint8_t* ram;
     int pc; /**< program counter */
+    int sp; /**< stack pointer */
 } bb_state_t;
 
 /******************************************************************************
--- src/byteboy.c
+++ src/byteboy.c
@@ -11,35 +11,36 @@
 #include <assert.h>
 #include <string.h> /* memset */
 
-  /******************************************************************************
-   * PROJECT INCLUDE FILES
-   ******************************************************************************/
+/******************************************************************************
+ * PROJECT INCLUDE FILES
+ ******************************************************************************/
 
 #include "byteboy.h"
 
-   /******************************************************************************
-    * DEFINES
-    ******************************************************************************/
+/******************************************************************************
+ * DEFINES
+ ******************************************************************************/
 
-    /******************************************************************************
-     * TYPEDEFS
-     ******************************************************************************/
+/******************************************************************************
+ * TYPEDEFS
+ ******************************************************************************/
 
-     /******************************************************************************
-      * LOCAL DATA DEFINITIONS
-      ******************************************************************************/
+/******************************************************************************
+ * LOCAL DATA DEFINITIONS
+ ******************************************************************************/
 
-      /******************************************************************************
-       * LOCAL FUNCTION PROTOTYPES
-       ******************************************************************************/
+/******************************************************************************
+ * LOCAL FUNCTION PROTOTYPES
+ ******************************************************************************/
 
-       /******************************************************************************
-        * FUNCTION BODIES
-        ******************************************************************************/
+/******************************************************************************
+ * FUNCTION BODIES
+ ******************************************************************************/
 
 static inline void bb_add(bb_state_t* bb, uint8_t a, uint8_t b)
 {
     int result = a + b;
+    bb->reg.a = (uint8_t)(result & 0xff);
 
     // ZERO FLAG
     if (result == 0)
@@ -51,15 +52,15 @@ static inline void bb_add(bb_state_t* bb, uint8_t a, uint8_t b)
         bb->reg.f &= ~BB_ZERO_FLAG;
     }
     // CARRY FLAG
-	if (result > 0xFF)
-	{
-		bb->reg.f |= BB_CARRY_FLAG;
-	}
-	else
-	{
-		bb->reg.f &= ~BB_CARRY_FLAG;
-	}
-	// HALF-CARRY FLAG
+    if (result > 0xFF)
+    {
+        bb->reg.f |= BB_CARRY_FLAG;
+    }
+    else
+    {
+        bb->reg.f &= ~BB_CARRY_FLAG;
+    }
+    // HALF-CARRY FLAG
     if ((bb->reg.a & 0xF) + (b & 0xF) > 0xF)
     {
         bb->reg.f |= BB_HALF_CARRY_FLAG;
@@ -70,19 +71,25 @@ static inline void bb_add(bb_state_t* bb, uint8_t a, uint8_t b)
     }
 }
 
+static inline void bb_add_carry(bb_state_t* bb, uint8_t a, uint8_t b)
+{
+    bb_add(bb, a, b + ((bb->reg.f & BB_CARRY_FLAG) ? 1 : 0));
+}
+
 int bb_vm(bb_state_t* bb)
 {
     uint8_t* code = bb->rom;
     uint8_t* ram = bb->ram;
+    int run = 1;
 
-
-    while (1)
+    while (run)
     {
         switch (code[bb->pc++])
         {
+        /* -- ADD -------------------------------------------- */
         case BB_OP_ADD_A_A:
             bb_add(bb, bb->reg.a, bb->reg.a);
-			break;
+            break;
         case BB_OP_ADD_A_B:
             bb_add(bb, bb->reg.a, bb->reg.b);
             break;
@@ -102,12 +109,44 @@ int bb_vm(bb_state_t* bb)
             bb_add(bb, bb->reg.a, bb->reg.l);
             break;
         case BB_OP_ADD_A_HL:
-            bb_add(bb, bb->reg.a, ram[bb->reg.hl & BB_MEM_MASK]);
+            bb_add(bb, bb->reg.a, ram[((bb->reg.h << 8) | bb->reg.l) & BB_MEM_MASK]);
             break;
         case BB_OP_ADD_A_N:
             bb_add(bb, bb->reg.a, code[bb->pc++]);
             break;
 
+        /* -- ADC -------------------------------------------- */
+        case BB_OP_ADC_A_A:
+            bb_add_carry(bb, bb->reg.a, bb->reg.a);
+            break;
+        case BB_OP_ADC_A_B:
+            bb_add_carry(bb, bb->reg.a, bb->reg.b);
+            break;
+        case BB_OP_ADC_A_C:
+            bb_add_carry(bb, bb->reg.a, bb->reg.c);
+            break;
+        case BB_OP_ADC_A_D:
+            bb_add_carry(bb, bb->reg.a, bb->reg.d);
+            break;
+        case BB_OP_ADC_A_E:
+            bb_add_carry(bb, bb->reg.a, bb->reg.e);
+            break;
+        case BB_OP_ADC_A_H:
+            bb_add_carry(bb, bb->reg.a, bb->reg.h);
+            break;
+        case BB_OP_ADC_A_L:
+            bb_add_carry(bb, bb->reg.a, bb->reg.l);
+            break;
+        case BB_OP_ADC_A_HL:
+            bb_add_carry(bb, bb->reg.a, ram[((bb->reg.h << 8) | bb->reg.l) & BB_MEM_MASK]);
+            break;
+        case BB_OP_ADC_A_N:
+            bb_add_carry(bb, bb->reg.a, code[bb->pc++]);
+            break;
+
+        case BB_OP_HALT:
+            run = 0;
+            break;
         default:
             assert(0);
         };
@@ -116,6 +155,19 @@ int bb_vm(bb_state_t* bb)
 
 int main(int argc, char** argv)
 {
+    bb_state_t bb = { 0 };
+    uint8_t code[128] = { BB_OP_ADC_A_HL, BB_OP_HALT };
+    uint8_t ram[128];
+
+    bb.rom = code;
+    bb.ram = ram;
+
+    bb.reg.a = 0x10;
+    ram[0x30] = 0x20;
+    bb.reg.f |= BB_CARRY_FLAG;
+    bb.reg.l = 0x30;
+
+    bb_vm(&bb);
 
     return 0;
 }
